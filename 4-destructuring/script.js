5bc2bedfebbbeb70064547b8e4a4d10fa54f41e2
Clementterme <clement73terme@gmail.com>
2026-04-23 17:01:48 +0200

    Exercice Destructuring 4.2 terminé

--- 4-destructuring/script.js
+++ 4-destructuring/script.js
@@ -1,13 +1,39 @@
 //4.1 Découverte
 
-const [a, b] = [1, 2, 3, 4];
+const [a, b, ...rest] = [1, 2, 3, 4];
 console.log(a);
 console.log(b);
+console.log(rest[0])
 
-const {first, last} = {first: 'Paul', last: 'Henta', age: 35};
+const {first, last, ...reste} = {first: 'Paul', last: 'Henta', age: 35};
 console.log(first);
 console.log(last);
+console.log(reste.age);
 
 //4.2 Application
+
+const perturbations = Object.values(data);
+
+console.log(perturbations);
+
+perturbations.forEach((perturbation) => {
+    const texte = perturbation.texte;
+    const dateDebut = perturbation.dateDebut;
+    const dateFin = perturbation.dateFin;
+
+    console.log("Texte :", texte);
+    console.log("Date début :", dateDebut);
+    console.log("Date fin :", dateFin);
+});
+
+let perturb = {
+    "texte": "C1 : Accident de tram"
+}
+
+function ajouterPerturbation(perturbation, objet) {
+    objet.perturbation = perturbation;
+}
+
+ajouterPerturbation(perturb, data);
 
 console.log(data);
