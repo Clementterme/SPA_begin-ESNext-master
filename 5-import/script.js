e2d2e88933154488becae23d2dd6ef16a1209563
Clementterme <clement.terme.simplon@gmail.com>
2026-04-24 12:06:11 +0200

    Exercice Import / Export terminé

--- 5-import/script.js
+++ 5-import/script.js
@@ -4,5 +4,16 @@ import {hello} from './functions.js';
 
 hello();
 
+import fonctionAnonyme, {showError} from './functions.js';
+
+showError();
+
+fonctionAnonyme();
+
 // 5-2 Export
 const [a, b] = [7, 9];
+
+import as from './math.js';
+
+sum(a, b);
+divide(a, b);
